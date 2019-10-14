bd1f76fd3a3a339891ac1bd7f3daf601fa5eec9a
Esteban <eecheverrya@uqvirtual.edu.co>
2019-10-14 18:53:58 -0500

    List of spaces comes from API

--- views/Space/index.js
+++ views/Space/index.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
 import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
-import ListItem from '../../components/ListItem'
+import ListItem from '../../components/ListItem';
+import axios from 'axios';
 
 class Index extends Component {
     
-    render(){
-        const DATA = [
-            {
-              id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-              title: 'First Item',
-            },
-            {
-              id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-              title: 'Second Item',
-            },
-            {
-              id: '58694a0f-3da1-471f-bd96-145571e29d72',
-              title: 'Third Item',
-            },
-        ];
+    constructor(props) {
+        super(props);
+        this.state = {
+            spaces: []
+        };
+    }
 
+    componentDidMount(){
+        axios.get('https://gestor-laboratorios.herokuapp.com/api/spaces')
+        .then((response) =>{
+            this.setState({
+                spaces: response.data.spaces
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    render(){
+        let {spaces} = this.state;
         return(
+            spaces.length > 0 &&
             <SafeAreaView style={styles.container}>
                 <FlatList
-                    data={DATA}
-                    renderItem={({ item }) => <ListItem title={item.title} />}
-                    keyExtractor={item => item.id}
+                    data={spaces}
+                    renderItem={({ item }) => <ListItem title={item.name} />}
+                    keyExtractor={item => item._id}
                 />
             </SafeAreaView>
         );
