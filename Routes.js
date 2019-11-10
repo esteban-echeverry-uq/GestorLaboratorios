968ec0d61c986708ca1843ce99d1971a9c89337c
Esteban Echeverry <eecheverrya@uqvirtual.edu.co>
2019-11-10 12:20:20 -0500

    Added currentUser logic

--- Routes.js
+++ Routes.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
-import Login from './views/Auth/Login'
-import SignUp from './views/Auth/SignUp'
-import Spaces from './views/Space/index'
-import ShowSpace from './views/Space/show'
-import SpaceForm from './views/Space/form'
-import RoomForm from './views/Room/Form'
+import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+import Login from './views/Auth/Login';
+import SignUp from './views/Auth/SignUp';
+import Spaces from './views/Space/index';
+import ShowSpace from './views/Space/show';
+import SpaceForm from './views/Space/form';
+import RoomForm from './views/Room/Form';
 
-const Routes = () => (
-    <Router>
-        <Scene key="root" style>
-            <Scene key="login" component={Login} title="Iniciar Sesión" />
-            <Scene key="signUp" component={SignUp} title="Crear Cuenta" />
+const SessionService = require('./services/sessionService');
+const sessionService = new SessionService();
 
-            <Scene key="spacesIndex" component={Spaces} title="Facultades" initial={true}/>
-            <Scene key="showSpace" component={ShowSpace} title />
-            <Scene key="createSpace" component={SpaceForm} title="Crear Espacio" />
-            <Scene key="editSpace" component={SpaceForm} title="Editar Espacio" />
+export default class SpaceForm extends Component {
+    state = {
+        currentUser: undefined
+    };
 
-            <Scene key="createRoom" component={RoomForm} title="Crear Sala" />
-        </Scene>
-    </Router>
-)
+    componentDidMount() {
+        sessionService.getCurrentUser().then(response => {
+            if (response.status === 'success') {
+                this.setState({ currentUser: response.currentUser });
+            }
+        });
+    }
 
-export default Routes
+    render() {
+        const { currentUser } = this.state;
+
+        return (
+            <Router>
+                <Scene key="root" style>
+                    <Scene key="login" component={Login} title="Iniciar Sesión" />
+                    <Scene key="signUp" component={SignUp} title="Crear Cuenta" />
+
+                    <Scene key="spacesIndex" component={Spaces} title="Facultades" initial={true} currentUser={currentUser} />
+                    <Scene key="showSpace" component={ShowSpace} title currentUser={currentUser} />
+                    <Scene key="createSpace" component={SpaceForm} title="Crear Espacio" currentUser={currentUser} />
+                    <Scene key="editSpace" component={SpaceForm} title="Editar Espacio" currentUser={currentUser} />
+
+                    <Scene key="createRoom" component={RoomForm} title="Crear Sala" currentUser={currentUser} />
+                </Scene>
+            </Router>
+        );
+    }
+}
