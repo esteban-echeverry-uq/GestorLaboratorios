f10c6ada2d261807ce7893e9d7d693cf9aa46be8
Esteban <eecheverrya@uqvirtual.edu.co>
2019-11-23 20:45:03 -0500

    Updated time format for dropdown

--- views/Reservation/dropDown.js
+++ views/Reservation/dropDown.js
@@ -1,51 +1,67 @@
 const dropDownData = [
     {
         value: '7',
+        label: '7:00'
     },
     {
         value: '8',
+        label: '8:00'
     },
     {
         value: '9',
+        label: '9:00'
     },
     {
         value: '10',
+        label: '10:00'
     },
     {
         value: '11',
+        label: '11:00'
     },
     {
         value: '12',
+        label: '12:00'
     },
     {
         value: '13',
+        label: '13:00'
     },
     {
         value: '14',
+        label: '14:00'
     },
     {
         value: '15',
+        label: '15:00'
     },
     {
         value: '16',
+        label: '16:00'
     },
     {
         value: '17',
+        label: '17:00'
     },
     {
         value: '18',
+        label: '18:00'
     },
     {
         value: '19',
+        label: '19:00'
     },
     {
         value: '20',
+        label: '20:00'
     },
     {
         value: '21',
+        label: '21:00'
     },
     {
         value: '22',
+        label: '22:00'
     }
 ]
 
